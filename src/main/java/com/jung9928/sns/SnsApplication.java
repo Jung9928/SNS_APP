05bb3a6a16436c111c1b4ae32dfa1ae45ecd48b0
Jung9928 <fg43irks@naver.com>
2023-11-25 16:11:17 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/com/jung9928/sns/SnsApplication.java
@@ -0,0 +1,15 @@
+package com.jung9928.sns;
+
+import lombok.ToString;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsApplication.class, args);
+	}
+
+}
